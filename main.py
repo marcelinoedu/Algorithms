d6c7c720758804c0099f439240dba45fb254cb84
Eduardo Marcelino <eduardoofficial12@gmail.com>
2023-09-28 00:00:26 -0300

    updates

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from Services.ClosingFinanceSheet import closing_finance
 
 def main():
     finance_sheet = load_finance_sheet()
-    # closing_finance(finance_sheet['values'])
-    for i, rows in enumerate(finance_sheet['values'][0:], start=1):
-        print(f'linha:{i}, dados: {rows}')
+    closing_finance(finance_sheet['values'])
+    # for i, rows in enumerate(finance_sheet['values'][0:], start=1):
+    #     print(f'linha:{i}, dados: {rows}')
 
 
 if __name__ == '__main__':

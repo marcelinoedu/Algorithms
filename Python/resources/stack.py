7ad6e93ebb8ef4d7bea887f4347cacc6166ab0fb
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-09-05 15:54:35 -0300

    ajuste na stack

--- Python/resources/stack.py
+++ Python/resources/stack.py
@@ -8,14 +8,12 @@ class Stack:
         self.items = []
         
     def push(self, item: Node) -> None:
-        
         self.items.insert(0,item)
     
     def pop(self) -> Node:
-        
         if self.is_empty():
             raise IndexError("A fila está vazia!")
-        return self.items.pop(-1)
+        return self.items.pop(0)
     
     def is_empty(self):
         
